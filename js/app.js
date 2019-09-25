70bae022b70b3acfc4a7cc719cc598d734b380cb
Emmanuel Yakubu <emmanuel.ovurevu@gmail.com>
2019-09-25 15:05:59 +0100

    refactored code to meet requirements

--- js/app.js
+++ js/app.js
@@ -1,5 +1,59 @@
-//listen for submit
-document.getElementById('nw-form').addEventListener('submit', calculateResults);
+//add asset and value to the assets list
+document.getElementById('add-asset-form').addEventListener('submit', function (e) {
+    //cache add asset modal UI vars
+    let assets = document.getElementById('assets');
+    let assetName = document.getElementById('asset-name');
+    let assetValue = document.getElementById('asset-value');
+
+    //create a div for append to assets list
+    let div = document.createElement('div');
+    div.className = 'form-group';
+    div.innerHTML = `<div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">${assetName.value}</span>
+                        </div>
+                        <input type="number" class="form-control asset" value="${assetValue.value}">
+                    </div>`;
+
+    //append the div
+    assets.appendChild(div);
+
+    //clear modal fields
+    assetName.value = "";
+    assetValue.value = "";
+
+    e.preventDefault();
+});
+
+//add liability and value to the liabilities list
+document.getElementById('add-liability-form').addEventListener('submit', function (e) {
+    //cache add asset modal UI vars
+    let liabilities = document.getElementById('liabilities');
+    let liabilityName = document.getElementById('liability-name');
+    let liabilityValue = document.getElementById('liability-value');
+
+    //create a div for append to assets list
+    let div = document.createElement('div');
+    div.className = 'form-group';
+    div.innerHTML = `<div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">${liabilityName.value}</span>
+                        </div>
+                        <input type="number" class="form-control liabilities" value="${liabilityValue.value}">
+                    </div>`;
+
+    //append the div
+    liabilities.appendChild(div);
+
+    //clear modal fields
+    liabilityName.value = "";
+    liabilityValue.value = "";
+
+    e.preventDefault();
+});
+
+//listen for calculate
+document.getElementById('calculate-button').addEventListener('click', calculateResults);
 
 //calculate results
 function calculateResults(e) {
